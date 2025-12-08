51f4fdc37633bb3ee6ad77c175d195ee60ed1ea4
yegecali <yemi.canchari@gmail.com>
2025-12-07 21:59:46 -0500

    refactor: Update App.tsx routing to use new pages structure
    - Import page components from ./pages folder (Home, AboutPage, PortfolioPage, ContactPage)
    - Remove old component imports (Navbar, Hero, Portafolio, AcercaDe, Contacto, Footer)
    - Simplify routing structure - pages now include their own layouts
    - Each page wraps content with MainLayout (includes Navbar and Footer)
    - Home page contains full Hero section with animations
    - Cleaner separation between routing and layout concerns

--- src/App.tsx
+++ src/App.tsx
@@ -6,28 +6,22 @@ import { DataLoaderWrapper } from "./DataLoaderWrapper";
 import { GlobalStyles } from "./styles/GlobalStyles";
 import { useThemeContext } from "./context/useThemeContext";
 import { ThemeProviderComponent } from "./context/ThemeContextProvider";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Portafolio from "./components/Portafolio";
-import AcercaDe from "./components/AcercaDe";
-import Contacto from "./components/Contacto";
-import Footer from "./components/Footer";
+import { Home, AboutPage, PortfolioPage, ContactPage } from "./pages";
 import ThemeToggle from "./components/ThemeToggle";
 
 function AppContent() {
   return (
     <DataLoaderWrapper>
       <Router basename="/portfolio/">
-        <Navbar />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Hero />} />
-            <Route path="/acerca" element={<AcercaDe />} />
-            <Route path="/portafolio" element={<Portafolio />} />
-            <Route path="/contacto" element={<Contacto />} />
-          </Routes>
-        </main>
-        <Footer />
+        <Routes>
+          {/* Home page with full Hero section */}
+          <Route path="/" element={<Home />} />
+
+          {/* Other pages with MainLayout (includes Navbar and Footer) */}
+          <Route path="/acerca" element={<AboutPage />} />
+          <Route path="/portafolio" element={<PortfolioPage />} />
+          <Route path="/contacto" element={<ContactPage />} />
+        </Routes>
         <ThemeToggle />
       </Router>
     </DataLoaderWrapper>
